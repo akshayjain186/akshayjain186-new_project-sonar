1c37dcdc80a48d1e7f36d85e6f0513a47f23964c
mega <mega.dubey@digiprima.co>
2024-12-07 11:49:30 +0530

    changes

--- backend/user-service/src/routes/userRoutes.js
+++ backend/user-service/src/routes/userRoutes.js
@@ -10,14 +10,13 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 
 // Endpoint to verify JWT token
-router.post('/verify', verifyToken, (req, res) => {
-    // If the token is valid, the user's ID will be available in req.user from the verifyToken middleware
-    res.status(200).json({ message: 'Token is valid', userId: req.userId });
-  });
+// router.post('/verify', verifyToken, (req, res) => {
+//     // If the token is valid, the user's ID will be available in req.user from the verifyToken middleware
+//     res.status(200).json({ message: 'Token is valid', userId: req.userId });
+//   });
 
   //Forgot Password
   router.post('/forgot-password',forgotPassword);
 
-  // router.get('/user-list',getAllUsers);
 
 module.exports = router;
