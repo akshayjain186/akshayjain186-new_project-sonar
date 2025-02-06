310c20c6f1e8549b39488e7553ec9dbe3537f5c0
Manish Kumbhkar <mkumar@spacepointe.com>
2025-02-06 22:42:07 +0530

    changes in port

--- frontend/GUI_TKY/ecosystem.config.js
+++ frontend/GUI_TKY/ecosystem.config.js
@@ -7,15 +7,15 @@ module.exports = {
         watch: true,
         env: {
           NODE_ENV: "development",
-          PORT: 7001, // Specify the port here
+          PORT: 7000, // Specify the port here
         },
         env_staging: {
           NODE_ENV: "staging",
-          PORT: 7002, // Port for staging
+          PORT: 7000, // Port for staging
         },
         env_production: {
           NODE_ENV: "production",
-          PORT: 7003, // Port for production
+          PORT: 7000, // Port for production
         },
       },
     ],
