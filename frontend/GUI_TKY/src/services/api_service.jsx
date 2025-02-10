d0f27c4d434ed0939ad506c5f50997c20cf7f9b3
ghanshyamDexterTL <ghanshyam.nagar@uniqgridcloud.com>
2025-02-10 15:08:48 +0530

    changes in code

--- frontend/GUI_TKY/src/services/api_service.jsx
+++ frontend/GUI_TKY/src/services/api_service.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 const token = '';
 
 // Apply base URL for axios
-const API_URL = "http://localhost:7001"   //import.meta.env.VITE_API_BASE_URL;
+const API_URL = "http://103.120.178.54:7001"   //import.meta.env.VITE_API_BASE_URL;
 
 const axiosApi = axios.create({
   baseURL: API_URL,
